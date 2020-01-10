7339c30a23ead27b7787693ff8df1bc9f0178206
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-10 13:57:19 -0300

    Adicionando terceiro desafio

---
+++ 003-desafio-propriedades.js
@@ -0,0 +1,25 @@
+/*
+ * Arquivo: desafio-propriedades.js
+ * Autor: Paulo Alves
+ * Descrição: solução do exercicio sobre o uso de propriedades
+ * Data: 10/01/2020
+*/
+
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0
+    },
+    computed: {
+        resultado(){
+            return this.valor == 37 ? 'Valor Igual' : 'Valor Diferente'
+        }
+    },
+    watch: {
+        resultado(){
+            setTimeout(() => {
+                this.valor = 0
+            }, 5000)
+        }
+    },
+});
