da4da1a28584091d40fb9757a7126c4ac5f059a8
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-10 16:38:16 -0300

    Adicionando quarto desafio

---
+++ 004-desafio-estilo.js
@@ -0,0 +1,44 @@
+/*
+ * Arquivo: 004-desafio-estilo.js
+ * Autor: Paulo Alves
+ * Descrição: solução do exercicio sobre o uso de estilos
+ * Data: 10/01/2020
+*/
+
+new Vue({
+	el: '#desafio',
+	data: {
+        classe1: 'destaque',
+        perigo: true,
+        classe3: '',
+        classe4: '',
+        cor5: '',
+        estilo5: {
+            width: '100px',
+            height: '100px',
+        },
+        width: '0',
+	},
+	methods: {
+		iniciarEfeito() {
+            setInterval(() => {
+                this.classe1 = this.classe1 == 'destaque' ? 'encolher' : 'destaque'
+            }, 2000)
+		},
+		iniciarProgresso() {
+            let valor = 0
+            const temporizador = setInterval(() => {
+                valor += 5
+                this.width = `${valor}%`
+                if(valor == 100) clearInterval(temporizador)
+            }, 500)
+        },
+        setPerigo(event){
+            if(event.target.value == "true"){
+                this.perigo = true
+            }else if(event.target.value == "false"){
+                this.perigo = false
+            }
+        }
+	}
+})
